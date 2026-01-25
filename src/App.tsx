881d343525d1c678db15e54c53f53e8f73b8c350
Mark Hartley <mark.hartley0929@gmail.com>
2026-01-24 23:23:19 -0500

    Add single-event registration API (SWA function)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PlayerRegistrationForm from "./components/PlayerRegistrationForm";
 
+const EVENT_ID = "b04de545-5aee-4403-86b1-03db1e5c4a86";
+
 export default function App() {
   return (
     <Routes>
@@ -11,7 +13,7 @@ export default function App() {
       {/* REGISTRATION PAGE */}
       <Route
         path="/register"
-        element={<PlayerRegistrationForm eventId="demo-event-id" />}
+        element={<PlayerRegistrationForm eventId={EVENT_ID} />}
       />
     </Routes>
   );
