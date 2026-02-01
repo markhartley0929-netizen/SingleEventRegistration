b5c4561e708e7f588ece56afbf323c2d52bcbda4
Mark Hartley <mark.hartley0929@gmail.com>
2026-02-01 13:07:50 -0500

    Remove Recaptcha

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,23 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+console.log("BOOTSTRAP: start");
+
+const rootEl = document.getElementById("root");
+console.log("BOOTSTRAP: root element", rootEl);
+
+if (!rootEl) {
+  throw new Error("Root element not found");
+}
+
+const root = ReactDOM.createRoot(rootEl);
+
+console.log("BOOTSTRAP: before render");
+
+root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
+
+console.log("BOOTSTRAP: after render");
