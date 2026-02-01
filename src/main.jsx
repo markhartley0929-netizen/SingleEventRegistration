37117dc1e7e4457d7e8b0cecac53eb676aeecdc8
Mark Hartley <mark.hartley0929@gmail.com>
2026-02-01 10:57:20 -0500

    Remove react-google-recaptcha-v3 provider from main

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <GoogleReCaptchaProvider
-      reCaptchaKey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}
-      scriptProps={{
-        async: true,
-        defer: true,
-      }}
-    >
-      <App />
-    </GoogleReCaptchaProvider>
+    <App />
   </BrowserRouter>
 );
-
