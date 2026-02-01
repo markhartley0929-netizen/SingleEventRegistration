8cbaadecae20ea9055ba3b9ef230f3fa0f41dd6e
Mark Hartley <mark.hartley0929@gmail.com>
2026-02-01 15:24:39 -0500

    Adding View Registered players css v11

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PlayerRegistrationForm from "./components/PlayerRegistrationForm";
 import PaymentSuccessPage from "./pages/PaymentSuccessPage";
@@ -8,23 +8,21 @@ const EVENT_ID = "b04de545-5aee-4403-86b1-03db1e5c4a86";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* HOME PAGE */}
-        <Route path="/" element={<HomePage />} />
+    <Routes>
+      {/* HOME PAGE */}
+      <Route path="/" element={<HomePage />} />
 
-        {/* REGISTRATION PAGE */}
-        <Route
-          path="/register"
-          element={<PlayerRegistrationForm eventId={EVENT_ID} />}
-        />
+      {/* REGISTRATION PAGE */}
+      <Route
+        path="/register"
+        element={<PlayerRegistrationForm eventId={EVENT_ID} />}
+      />
 
-        {/* REGISTERED PLAYERS PAGE */}
-        <Route path="/registrations" element={<RegisteredPlayers />} />
+      {/* REGISTERED PLAYERS PAGE */}
+      <Route path="/registrations" element={<RegisteredPlayers />} />
 
-        {/* PAYPAL SUCCESS RETURN */}
-        <Route path="/payment-success" element={<PaymentSuccessPage />} />
-      </Routes>
-    </BrowserRouter>
+      {/* PAYPAL SUCCESS RETURN */}
+      <Route path="/payment-success" element={<PaymentSuccessPage />} />
+    </Routes>
   );
 }
